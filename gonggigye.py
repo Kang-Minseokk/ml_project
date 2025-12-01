4c6a89f563f5bcb62ac3991e07b6800de39a1945
Kang-Minseokk <dreampilot920@naver.com>
2025-12-01 20:24:35 +0900

    fix: compare logic for classify [Vertical vs. Horizontal] modified

--- gonggigye.py
+++ gonggigye.py
@@ -244,9 +244,9 @@ class Minseok:
         x_list, y_list, z_list = Minseok.make_list(file_path)
         x_range, y_range, z_range = Minseok.check_range(x_list, y_list, z_list)
 
-        if (z_range < x_range) and (z_range < y_range) :        
+        if (y_range > x_range) and (y_range > z_range) : # Fix: 멘토님의 피드백을 받고 min에서 max로 수정
             return "horizontal"
-        elif (y_range < x_range) and (y_range < z_range) :
+        elif (z_range > x_range) and (z_range > y_range) : # Fix: 멘토님의 피드백을 받고 min에서 max로 수정
             return "vertical"
         else :
             return "unknown"
